ccb0923b041bf1adfe80cfb2e93a42271c57b630
Deylon <deyloncarlo@gmail.com>
2018-06-26 13:27:07 -0300

    criando função hash

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <stdbool.h>
 #include <termios.h>
 #include <unistd.h>
+#include <string.h>
 
-
-#define TAMANHO_TABELA_HASH 127
+#define TAMANHO_TABELA_HASH 10124
 
 struct palavra
 {
@@ -21,13 +21,15 @@ int getch(void);
 
 int getche(void);
 
-bool carregarDicionario(char *p_nomeArquivo);
+bool carregarDicionario(char *p_nomeArquivo, Palavra *p_tabelaHash, int p_tamanhoPrefixo);
 
 bool usuarioApertouEnter(char p_caracter);
 
 bool usuarioApertouBackspace(char p_caracter);
 
 bool numeroLetrasIncorreto(int p_numeroLetras, int p_numeroPermitido);
+
+int funcaoHash(char *p_prefixo, int p_fator);
 
 int main(int p_argc,char **p_argv)
 {
@@ -40,7 +42,16 @@ int main(int p_argc,char **p_argv)
     printf("\tAutocomplete\n");
     printf("==============================\n");
 
-    carregarDicionario(p_argv[1]);
+    int v_tamanhoPrefixo = atoi(p_argv[2]);
+    printf("Tamanho prefixo para indexar: %d\n", v_tamanhoPrefixo);
+
+    //Crinado tabela Hash
+    Palavra *tabelaHash = malloc(sizeof(Palavra) * TAMANHO_TABELA_HASH);
+    Palavra v_ola;
+    v_ola.chave = "Ola";
+    tabelaHash[0] = v_ola;
+    carregarDicionario(p_argv[1], tabelaHash, v_tamanhoPrefixo);
+
 
     int v_numeroMaximoIndexacao = TAMANHO_TABELA_HASH - 1;
     int v_numeroLetras = 0;
@@ -48,13 +59,6 @@ int main(int p_argc,char **p_argv)
     char v_char;
     char v_busca[v_numeroMaximoIndexacao];
     v_busca[v_numeroMaximoIndexacao] = '\0'; // Removendo caracter que identificar uma string ('@')
-
-    //Crinado tabela Hash
-    Palavra *tabelaHash = malloc(sizeof(Palavra) * TAMANHO_TABELA_HASH);
-    Palavra v_ola;
-    v_ola.chave = "Ola";
-    tabelaHash[0] = v_ola;
-    printf("%s", tabelaHash[0].chave);
 
     while(v_usuarioNaoApertouEnter)
     {
@@ -159,8 +163,13 @@ bool numeroLetrasIncorreto(int p_numeroLetras, int p_numeroPermitido)
     return false;
 }
 
-bool carregarDicionario(char *p_nomeArquivo)
+
+/**
+ * Função que carrega o dicionário de palávras 
+ */
+bool carregarDicionario(char *p_nomeArquivo, Palavra *p_tabelaHash, int p_tamanhoPrefixo)
 {
+    printf("%s", p_tabelaHash[0].chave);
     printf("\n[Início] Carregando dicionário...\n");
     
     FILE *v_dicionario = fopen(p_nomeArquivo, "r");
@@ -171,14 +180,51 @@ bool carregarDicionario(char *p_nomeArquivo)
         return EXIT_FAILURE;
     }
 
+//subbuff[4] = '\0';
     printf("\n\nPalavras: \n");
     printf("========");
+    
     while(fscanf(v_dicionario, "%s", v_palavra) != EOF)
     {
-        printf("\n%s", v_palavra);
+        //printf("\n%s", v_palavra);
+        if(p_tamanhoPrefixo >= strlen(v_palavra))
+        {
+            char v_aux[strlen(v_palavra)];
+            memcpy(v_aux, v_palavra, strlen(v_palavra));
+            printf("\nPrefixo encontrado: %s ", v_aux);
+            funcaoHash(v_aux, strlen(v_palavra));
+        }
+        else 
+        {
+            char v_prefixo[p_tamanhoPrefixo];
+            memcpy( v_prefixo, v_palavra, p_tamanhoPrefixo );
+            printf("\nIgual Prefixo encontrado: %s ", v_prefixo);
+            funcaoHash(v_prefixo,p_tamanhoPrefixo);
+        }
+        //strncpy(v_prefixo,v_palavra , 4);
+        //strcpy(v_prefixo, v_palavra);
+        
+        
     }
     printf("\n\n");
     fclose(v_dicionario);
 
     printf("\n[Fim] Carregando dicionário...\n");
+}
+
+int funcaoHash(char *p_prefixo, int p_fator)
+{
+    int v_hash = 0;
+    //printf("\nCalculando Hash");
+    int v_tamanho = strlen(p_prefixo);
+    int v_indice;
+    int v_decresce = p_fator;
+    for(v_indice = 0; v_indice < v_tamanho; v_indice++)
+    {
+        //printf("\nChar :  %c\n", p_prefixo[v_indice]);
+        v_hash += (v_decresce*v_decresce*v_decresce)*( ((int)p_prefixo[v_indice]) / (p_fator) );
+        v_decresce--;
+    }
+    printf(" Funcao Hash calculada: %d\n", v_hash);
+    return v_hash;
 }
